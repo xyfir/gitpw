1f4d2d23533d8423f1deb8c88850008bcf0b0b96
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-06-16 21:37:38 -0700

    Add Buffer for isomorphic-git

--- tiperite/App.tsx
+++ tiperite/App.tsx
@@ -1,4 +1,7 @@
 import 'react-native-gesture-handler';
+import { Buffer } from 'buffer';
+
+global.Buffer = Buffer;
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
