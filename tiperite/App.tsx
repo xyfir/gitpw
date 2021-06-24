3e910d9f48cc0bd0ea00d0d95998d90d217fd5fe
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-06-23 20:51:30 -0700

    Use WebExecutorHost

--- tiperite/App.tsx
+++ tiperite/App.tsx
@@ -4,6 +4,7 @@ import { Buffer } from 'buffer';
 global.Buffer = Buffer;
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { WebExecutorHost } from './components/WebExecutorHost';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import { Navigation } from './navigation';
@@ -16,8 +17,9 @@ export default function App() {
 
   return isLoadingComplete ? (
     <SafeAreaProvider>
-      <Navigation colorScheme={colorScheme} />
       <StatusBar />
+      <Navigation colorScheme={colorScheme} />
+      <WebExecutorHost />
     </SafeAreaProvider>
   ) : null;
 }
