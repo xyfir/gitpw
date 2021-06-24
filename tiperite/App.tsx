bc8c8ec6dc79e789afe3c8dd7eff6a88e74fe7ee
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-06-23 20:52:11 -0700

    Rename file

--- tiperite/App.tsx
+++ tiperite/App.tsx
@@ -7,7 +7,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { WebExecutorHost } from './components/WebExecutorHost';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
-import { Navigation } from './navigation';
+import { Navigation } from './navigation/Navigation';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
