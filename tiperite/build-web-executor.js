ad3622c77c46fbada045787bcceb2ea305146706
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-06-24 22:37:04 -0700

    Use base64

--- tiperite/build-web-executor.js
+++ tiperite/build-web-executor.js
@@ -15,45 +15,14 @@ const fs = require('fs').promises;
     await fs.writeFile(
       path.resolve(__dirname, './constants/WebExecutorHTML.ts'),
       [
-        "export const WebExecutorHTML = '",
+        'export const WebExecutorHTML = `',
         html
-          .replace(
-            '/* %ISOGIT% */',
-            isogitJS.replace(/\\/g, '\\').replace(/'/g, "\\'"),
-          )
-          .replace(/\n/g, '\\'),
-        "';",
-      ].join(''),
+          .replace('%ISOGIT%', Buffer.from(isogitJS).toString('base64'))
+          .trim(),
+        '`;',
+      ].join('\n'),
     );
   } catch (err) {
     console.error(err);
   }
 })();
-
-// const path = require('path');
-// const fs = require('fs').promises;
-
-// (async () => {
-//   try {
-//     const isogitJS = await fs.readFile(
-//       path.resolve(__dirname, './node_modules/isomorphic-git/index.umd.min.js'),
-//       'utf8',
-//     );
-//     const html = await fs.readFile(
-//       path.resolve(__dirname, './constants/WebExecutor.html'),
-//       'utf8',
-//     );
-
-//     await fs.writeFile(
-//       path.resolve(__dirname, './constants/WebExecutorHTML.ts'),
-//       [
-//         'export const WebExecutorHTML = `',
-//         html.replace('/* %ISOGIT% */', isogitJS.replace(/`/g, '\\`')),
-//         '`;',
-//         '',
-//       ].join('\n'),
-//     );
-//   } catch (err) {
-//     console.error(err);
-//   }
-// })();
