b816e8ef911bf2d1c2d2745d2e1be44da4a8e19a
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-06-25 21:16:37 -0700

    Document

--- tiperite/scripts/build-web-executor.js
+++ tiperite/scripts/build-web-executor.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const fs = require('fs').promises;
 
+/**
+ * Loading local files in WebView gets to be really miserable dealing with
+ *  native assets, especially across multiple platforms. This build script
+ *  allows us to dynamically generate TypeScript to load all our assets in JS.
+ */
 (async () => {
   try {
+    // Import the UMD build of isomorphic-git
     const isogitJS = await fs.readFile(
       path.resolve(
         __dirname,
@@ -10,11 +16,14 @@ const fs = require('fs').promises;
       ),
       'utf8',
     );
+
+    // Get the HTML template we'll inject into WebExecutorHost
     const html = await fs.readFile(
       path.resolve(__dirname, '../WebExecutor.html'),
       'utf8',
     );
 
+    // Build a .ts file that'll export the HTML to inject into WebExecutor
     await fs.writeFile(
       path.resolve(__dirname, '../constants/WebExecutorHTML.ts'),
       [
