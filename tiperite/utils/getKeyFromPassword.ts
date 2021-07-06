941748b1c1e1b1e866369a17673a3e3715caa636
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-07-05 17:05:17 -0700

    Improve performance

--- tiperite/utils/getKeyFromPassword.ts
+++ tiperite/utils/getKeyFromPassword.ts
@@ -8,23 +8,65 @@ export function getKeyFromPassword(
   pass = Buffer.from(pass, 'utf8').toString('base64');
 
   return WebExecutor.exec<string>(`
-    async function sha512(message) {
-      const hashBuffer = await crypto.subtle.digest(
-        'SHA-512',
-        new TextEncoder().encode(message)
-      );
-      const hashHex = Array.from(new Uint8Array(hashBuffer)).map(
-        b => b.toString(16).padStart(2, '0')
-      ).join('');
-      return hashHex;
+    function bytesToHexString(bytes) {
+      if (!bytes) return null;
+
+      bytes = new Uint8Array(bytes);
+      const hexBytes = [];
+
+      for (var i = 0; i < bytes.length; ++i) {
+        let byteString = bytes[i].toString(16);
+        if (byteString.length < 2) byteString = '0' + byteString;
+        hexBytes.push(byteString);
+      }
+
+      return hexBytes.join('');
     }
 
-    let key = '${pass}';
-
-    for (let i = 0; i < ${itr}; i++) {
-      key = await sha512('${salt}' + key);
+    function toBase64(arr) {
+      return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.addEventListener(
+          'load',
+          () => resolve(reader.result.split(',')[1]),
+          false
+        );
+        reader.readAsDataURL(new Blob([arr]));
+      });
     }
 
-    return key;
+    const passphraseKey = new TextEncoder('utf-8').encode(btoa('${pass}'));
+    const saltBuffer = crypto.getRandomValues(new Uint8Array(16));
+
+    const key = await crypto.subtle.importKey(
+      'raw',
+      passphraseKey,
+      { name: 'PBKDF2' },
+      false,
+      ['deriveBits', 'deriveKey']
+    );
+
+    const derivedKey = await crypto.subtle.deriveKey(
+      {
+        iterations: ${itr},
+        salt: saltBuffer,
+        hash: 'SHA-512',
+        name: 'PBKDF2',
+      },
+      key,
+      {
+        length: 256,
+        name: 'AES-GCM',
+      },
+      true,
+      ['encrypt', 'decrypt']
+    );
+
+    const keyBuffer = await crypto.subtle.exportKey('raw', derivedKey);
+
+    return {
+      salt: await toBase64(saltBuffer),
+      key: await toBase64(keyBuffer),
+    }
   `).promise;
 }
