6d93201fd2fa14d67296410937859524dc5f94e9
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-08-23 22:38:38 -0700

    Refactor

--- tiperite/web/NativeProxy.ts
+++ tiperite/web/NativeProxy.ts
@@ -4,28 +4,28 @@ export class NativeProxy {
   private static responses: Record<number, (payload: unknown) => void> = {};
   private static requestId = 0;
 
-  private static async collect(iterable: any) {
+  private static mergeRequestBodyArrays(arrays: Uint8Array[]): Uint8Array {
     let size = 0;
-    const buffers: any[] = [];
-    // This will be easier once `for await ... of` loops are available.
-    for await (const value of iterable) {
-      buffers.push(value);
+    for (const value of arrays) {
       size += value.byteLength;
     }
-    const result = new Uint8Array(size);
+
+    const merged = new Uint8Array(size);
     let nextIndex = 0;
-    for (const buffer of buffers) {
-      result.set(buffer, nextIndex);
-      nextIndex += buffer.byteLength;
+
+    for (const arr of arrays) {
+      merged.set(arr, nextIndex);
+      nextIndex += arr.byteLength;
     }
-    return result;
+
+    return merged;
   }
 
   /** Convert uint8array to base64url */
-  private static convertRequestBody(body: any) {
-    return NativeProxy.collect(body).then(
-      (body) => `data:*/*;base64,${Buffer.from(body).toString('base64')}`,
-    );
+  private static convertRequestBody(body: Uint8Array[]): string {
+    return `data:*/*;base64,${Buffer.from(
+      NativeProxy.mergeRequestBodyArrays(body),
+    ).toString('base64')}`;
   }
 
   /** Convert base64url to uint8array */
@@ -48,41 +48,39 @@ export class NativeProxy {
   }: {
     headers: Record<string, string>;
     method: string;
-    body: any;
+    body?: Uint8Array[];
     url: string;
   }): Promise<any> {
-    return (
-      body ? NativeProxy.convertRequestBody(body) : Promise.resolve()
-    ).then((body) => {
-      return new Promise((resolve, reject) => {
-        // Listen for RN's response
-        const id = NativeProxy.requestId++;
-        NativeProxy.responses[id] = (payload: any) => {
-          delete NativeProxy.responses[id];
+    const base64body = body ? NativeProxy.convertRequestBody(body) : undefined;
 
-          if (payload.error) return reject(payload.error);
+    return new Promise((resolve, reject) => {
+      // Listen for RN's response
+      const id = NativeProxy.requestId++;
+      NativeProxy.responses[id] = (payload: any) => {
+        delete NativeProxy.responses[id];
 
-          (payload.response.body
-            ? NativeProxy.convertResponseBody(payload.response.body)
-            : Promise.resolve()
-          ).then((responseBody) => {
-            resolve({
-              statusMessage: payload.response.statusMessage,
-              statusCode: payload.response.statusCode,
-              headers: payload.response.headers,
-              method: payload.response.method,
-              body: responseBody,
-              url: payload.response.url,
-            });
+        if (payload.error) return reject(payload.error);
+
+        (payload.response.body
+          ? NativeProxy.convertResponseBody(payload.response.body)
+          : Promise.resolve()
+        ).then((responseBody) => {
+          resolve({
+            statusMessage: payload.response.statusMessage,
+            statusCode: payload.response.statusCode,
+            headers: payload.response.headers,
+            method: payload.response.method,
+            body: responseBody,
+            url: payload.response.url,
           });
-        };
+        });
+      };
 
-        // Send to RN
-        const payload = { headers, body, method, url };
-        window.ReactNativeWebView.postMessage(
-          JSON.stringify({ payload, type: 'fetch', id }),
-        );
-      });
+      // Send to RN
+      const payload = { headers, body: base64body, method, url };
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({ payload, type: 'fetch', id }),
+      );
     });
   }
 }
