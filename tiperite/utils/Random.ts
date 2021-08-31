a1565084e019fb744d36a8ee517d862476f09300
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-08-30 21:01:32 -0700

    Add number generators

--- tiperite/utils/Random.ts
+++ tiperite/utils/Random.ts
@@ -1,7 +1,32 @@
 import { v4 as uuidv4 } from 'uuid';
+import { WebExecutor } from './WebExecutor';
 import { UUID } from '../types';
 
+/**
+ * A utility class for generating cryptographically secure data
+ */
 export class Random {
+  /**
+   * Returns an integer between `min` (inclusive) and `max` (exclusive)
+   */
+  public static async integer(min: number, max: number): Promise<number> {
+    const float = await this.float();
+    return Math.floor(float * (max - min) + min);
+  }
+
+  /**
+   * A cryptographically secure replacement for `Math.random()`. Returns a
+   *  number between `0` (inclusive) and `1` (exclusive).
+   */
+  public static float(): Promise<number> {
+    return WebExecutor.exec<number>(/* js */ `
+      return crypto.getRandomValues(new Uint32Array(1))[0] / Math.pow(2, 32);
+    `).promise;
+  }
+
+  /**
+   * Returns a UUID4 string
+   */
   public static uuid(): UUID {
     return uuidv4();
   }
