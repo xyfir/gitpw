e9227eed54204c2bc79c1e62c111f66af9d4ffc8
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-08-31 20:45:57 -0700

    Add BootFile

---
+++ tiperite/utils/BootFile.ts
@@ -0,0 +1,85 @@
+import * as SecureStore from 'expo-secure-store';
+import { KeyDeriver } from '../utils/KeyDeriver';
+import { Random } from '../utils/Random';
+import Constants from 'expo-constants';
+import { AES } from './AES';
+import { FS } from './FS';
+import {
+  TiperiteVersion,
+  EncryptedString,
+  BootFileData,
+  HexString,
+} from '../types';
+
+/**
+ * Read and write to `/boot.json`
+ */
+export class BootFile {
+  /**
+   * Key name in `SecureStore` for saving passkey
+   */
+  private static SECURESTORE_KEY: 'boot-passkey' = 'boot-passkey';
+  /**
+   * The boot passkey is used to encrypt `/boot.json` and is also used as the
+   *  encryption key (or part of it) for `/device.json`
+   */
+  private static passkey: HexString | null = null;
+  /**
+   * The path on the filesystem
+   */
+  private static PATH: '/boot.json' = '/boot.json';
+
+  /**
+   * Returns the boot passkey after generating the key if needed
+   */
+  private static async getPasskey(): Promise<HexString> {
+    if (this.passkey !== null) return this.passkey;
+
+    // Get previously generated passkey and return if available
+    this.passkey = await SecureStore.getItemAsync(this.SECURESTORE_KEY);
+    if (this.passkey) return this.passkey;
+
+    // Generate a passkey
+    const salt = await KeyDeriver.generateSalt();
+    const itr = await Random.integer(50000, 100000);
+    this.passkey = await KeyDeriver.deriveKey(Random.uuid(), salt, itr);
+
+    // Save and return passkey
+    await SecureStore.setItemAsync(this.SECURESTORE_KEY, this.passkey);
+    return this.passkey;
+  }
+
+  /**
+   * Returns a default `BootFileData` object
+   */
+  private static getDefaultData(): BootFileData {
+    return {
+      hasDevicePassword: false,
+      passwordLength: null,
+      passwordType: null,
+      version: Constants.nativeAppVersion as TiperiteVersion,
+    };
+  }
+
+  /**
+   * Returns the `BootFileData` object after initializing the file if needed
+   */
+  public static async getData(): Promise<BootFileData> {
+    // Read file and passkey
+    const ciphertext: EncryptedString | null = await FS.readFile(this.PATH);
+    const passkey = await this.getPasskey();
+
+    // Initialize file with default data
+    if (ciphertext === null) {
+      const data = this.getDefaultData();
+      const ciphertext_ = await AES.encrypt(JSON.stringify(data), passkey);
+      await FS.writeFile(this.PATH, ciphertext_);
+      return data;
+    }
+    // Decrypt and return existing data
+    else {
+      const plaintext = await AES.decrypt(ciphertext, passkey);
+      return JSON.parse(plaintext) as BootFileData;
+    }
+  }
+}
