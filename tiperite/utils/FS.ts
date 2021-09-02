507340a94b61034eed31e1ce9ba606c19101c7af
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-09-01 20:46:55 -0700

    Add generic

--- tiperite/utils/FS.ts
+++ tiperite/utils/FS.ts
@@ -22,8 +22,8 @@ export class FS {
   /**
    * Read a file as a string or return `null` if the file doesn't exist
    */
-  public static readFile(path: string): Promise<string | null> {
-    return WebExecutor.exec<string | null>(
+  public static readFile<T extends string>(path: string): Promise<T | null> {
+    return WebExecutor.exec<T | null>(
       /* js */ `
         return await window.fs.readFile(params.path, 'utf8').catch((e) => {
           if (e.code == 'ENOENT') return null;
