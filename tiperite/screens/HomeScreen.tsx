df0c5c770e320ae261c60bd15fe49cb8b840ea50
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-09-06 16:32:21 -0700

    Fix

--- tiperite/screens/HomeScreen.tsx
+++ tiperite/screens/HomeScreen.tsx
@@ -1,22 +1,31 @@
-import { StyleSheet, TextInput, Button, Text, View } from 'react-native';
-import { BootFileData } from '../types';
+import { StyleSheet, TextInput, Button, Text, View, Alert } from 'react-native';
+import { DeviceFileData, BootFileData } from '../types';
+import { DeviceFile } from '../utils/DeviceFile';
 import { BootFile } from '../utils/BootFile';
+import { FS } from '../utils/FS';
 import React from 'react';
 
 export function HomeScreen(): JSX.Element | null {
+  const [deviceFileData, setDeviceFileData] = React.useState<DeviceFileData>();
   const [configPasscode, setConfigPasscode] = React.useState(false);
   const [authenticated, setAuthenticated] = React.useState(false);
   const [bootFileData, setBootFileData] = React.useState<BootFileData>();
-  const [passcode, setPasscode] = React.useState<string>();
+  const [passcode, setPasscode] = React.useState('');
 
   function onDisablePasscode(): void {
     const data: BootFileData = {
       ...bootFileData,
       firstLaunch: false,
     } as BootFileData;
-    BootFile.setData(data);
-    setAuthenticated(true);
-    setBootFileData(data);
+
+    BootFile.setData(data)
+      .then(() => DeviceFile.unlock(passcode))
+      .then(() => {
+        setDeviceFileData(DeviceFile.getData());
+        setBootFileData(data);
+        setConfigPasscode(false);
+        setAuthenticated(true);
+      });
   }
 
   function onEnablePasscode(): void {
@@ -31,13 +40,33 @@ export function HomeScreen(): JSX.Element | null {
       passwordType: 'number',
       firstLaunch: false,
     } as BootFileData;
-    BootFile.setData(data);
-    setAuthenticated(true);
-    setBootFileData(data);
+
+    BootFile.setData(data)
+      .then(() => DeviceFile.unlock(passcode))
+      .then(() => {
+        setDeviceFileData(DeviceFile.getData());
+        setBootFileData(data);
+        setConfigPasscode(false);
+        setAuthenticated(true);
+      });
   }
 
   function onUnlock(): void {
-    //
+    DeviceFile.unlock(passcode)
+      .then(() => {
+        setDeviceFileData(DeviceFile.getData());
+        setAuthenticated(true);
+      })
+      .catch(() => Alert.alert('Incorrect passcode!'));
+  }
+
+  function onReset(): void {
+    FS.unlink('/device.json')
+      .then(() => console.log('reset device'))
+      .catch((e) => console.error('reset device', e));
+    FS.unlink('/boot.json')
+      .then(() => console.log('reset boot'))
+      .catch((e) => console.error('reset boot', e));
   }
 
   React.useEffect(() => {
@@ -55,6 +84,7 @@ export function HomeScreen(): JSX.Element | null {
         onChangeText={setPasscode}
         keyboardType="numeric"
         value={passcode}
+        style={{ backgroundColor: 'gray', width: 200 }}
       />
 
       <Button onPress={onDisablePasscode} title="Cancel" />
@@ -63,6 +93,12 @@ export function HomeScreen(): JSX.Element | null {
   ) : authenticated ? (
     <View style={styles.root}>
       <Text>Welcome!</Text>
+
+      <Button onPress={onReset} title="Reset" />
+
+      <Text>{JSON.stringify(bootFileData, null, 2)}</Text>
+
+      <Text>{JSON.stringify(deviceFileData, null, 2)}</Text>
     </View>
   ) : bootFileData.firstLaunch ? (
     <View style={styles.root}>
@@ -82,6 +118,7 @@ export function HomeScreen(): JSX.Element | null {
         returnKeyType="done"
         onChangeText={setPasscode}
         keyboardType="numeric"
+        style={{ backgroundColor: 'gray', width: 200 }}
         value={passcode}
       />
 
@@ -91,5 +128,9 @@ export function HomeScreen(): JSX.Element | null {
 }
 
 const styles = StyleSheet.create({
-  root: {},
+  root: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
 });
