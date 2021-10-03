92f1a72d4a032f2a70df36942f559770c5016132
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-10-02 17:47:01 -0700

    Add impactHaptic prop

--- tiperite/components/TrButton.tsx
+++ tiperite/components/TrButton.tsx
@@ -1,7 +1,9 @@
 import { useTheme } from '../hooks/useTheme';
+import { Haptics } from '../utils/Haptics';
 import React from 'react';
 import {
   TouchableOpacityProps,
+  GestureResponderEvent,
   ActivityIndicator,
   TouchableOpacity,
   Text,
@@ -11,14 +13,17 @@ export interface TrButtonProps
   extends Omit<TouchableOpacityProps, 'activeOpacity'> {
   startAdornment?: JSX.Element;
   endAdornment?: JSX.Element;
+  impactHaptic?: undefined | 'medium' | 'heavy' | 'light';
   title?: string;
   busy?: boolean;
 }
 
 export function TrButton({
   startAdornment,
+  impactHaptic = 'light',
   endAdornment,
   disabled,
+  onPress,
   style,
   title,
   busy,
@@ -26,10 +31,20 @@ export function TrButton({
 }: TrButtonProps): JSX.Element {
   const theme = useTheme('TrButton');
 
+  /**
+   * Capture press in order to provide haptic feedback
+   */
+  function onInternalPress(e: GestureResponderEvent): void {
+    if (disabled || busy) return;
+    if (impactHaptic) Haptics.impact(impactHaptic);
+    if (onPress) onPress(e);
+  }
+
   return (
     <TouchableOpacity
       activeOpacity={disabled ? 0.5 : 0.2}
       disabled={disabled || busy}
+      onPress={onInternalPress}
       style={[theme.root, { opacity: disabled ? 0.5 : 1 }, style]}
       key={disabled ? 0 : 1}
       {...props}
