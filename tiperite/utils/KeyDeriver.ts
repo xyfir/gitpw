d39fa856a3c06c52a68c4e4f8473d1b102abc0d3
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-10-11 18:43:32 -0700

    Merge web/native code

--- tiperite/utils/KeyDeriver.ts
+++ tiperite/utils/KeyDeriver.ts
@@ -1,10 +1,7 @@
-import { WebExecutor } from './WebExecutor';
 import { HexString } from '../types';
 
 /**
  * Derive key from a user-supplied password
- *
- * @see KeyDeriverWeb
  */
 export class KeyDeriver {
   /**
@@ -20,29 +17,54 @@ export class KeyDeriver {
   /**
    * Generate a random 16-byte salt and convert to string
    */
-  public static generateSalt(): Promise<string> {
-    return WebExecutor.exec<string>(/* js */ `
-      return KeyDeriverWeb.generateSalt();
-    `).promise;
+  public static generateSalt(): string {
+    let salt = '';
+    crypto.getRandomValues(new Uint8Array(16)).forEach((byte) => {
+      salt += String.fromCharCode(byte);
+    });
+    return salt;
   }
 
   /**
    * Uses web's `SubtleCrypto` PBKDF2 interface to convert a password to a key
    */
-  public static deriveKey(
-    pass: string,
-    salt: string,
-    itr: number,
-  ): Promise<HexString> {
-    return WebExecutor.exec<string>(
-      /* js */ `
-        return KeyDeriverWeb.deriveKey(params);
-      `,
+  public static async deriveKey({
+    pass,
+    salt,
+    itr,
+  }: {
+    pass: string;
+    salt: string;
+    itr: number;
+  }): Promise<HexString> {
+    const passBuffer = new TextEncoder().encode(pass);
+    const saltBuffer = new TextEncoder().encode(salt);
+
+    const key = await crypto.subtle.importKey(
+      'raw',
+      passBuffer,
+      { name: 'PBKDF2' },
+      false,
+      ['deriveBits', 'deriveKey'],
+    );
+
+    const derivedKey = await crypto.subtle.deriveKey(
       {
-        pass,
-        salt,
-        itr,
+        iterations: +itr,
+        salt: saltBuffer,
+        hash: 'SHA-512',
+        name: 'PBKDF2',
       },
-    ).promise;
+      key,
+      {
+        length: 256,
+        name: 'AES-GCM',
+      },
+      true,
+      ['encrypt', 'decrypt'],
+    );
+
+    const keyBuffer = await crypto.subtle.exportKey('raw', derivedKey);
+    return Buffer.from(keyBuffer).toString('hex');
   }
 }
