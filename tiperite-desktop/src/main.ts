4ff3c4e19760ad74b4a75d4bd495837abb9e3b68
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-10-24 16:33:08 -0700

    Bypass CORS

--- tiperite-desktop/src/main.ts
+++ tiperite-desktop/src/main.ts
@@ -6,11 +6,29 @@ function main(): void {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
-    height: 600,
-    width: 800,
+    height: 1080,
+    width: 1920,
   });
 
-  win.loadFile('index.html');
+  // win.loadFile('index.html');
+  win.loadURL('http://localhost:19006');
+
+  win.webContents.openDevTools();
+
+  // Bypass CORS
+  win.webContents.session.webRequest.onBeforeSendHeaders(
+    (details, callback) => {
+      callback({ requestHeaders: { Origin: '*', ...details.requestHeaders } });
+    },
+  );
+  win.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        'Access-Control-Allow-Origin': ['*'],
+        ...details.responseHeaders,
+      },
+    });
+  });
 }
 
 app.whenReady().then(main).catch(console.error);
