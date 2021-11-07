6171155d2c7290b32ecf67851e20604b33b2017d
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-11-06 20:33:08 -0700

    Remove haptics

--- tiperite/components/TrButton.tsx
+++ tiperite/components/TrButton.tsx
@@ -1,9 +1,7 @@
 import { useTheme } from '../hooks/useTheme';
-import { Haptics } from '../utils/Haptics';
 import React from 'react';
 import {
   TouchableOpacityProps,
-  GestureResponderEvent,
   ActivityIndicator,
   TouchableOpacity,
   Text,
@@ -13,14 +11,12 @@ export interface TrButtonProps
   extends Omit<TouchableOpacityProps, 'activeOpacity'> {
   startAdornment?: JSX.Element;
   endAdornment?: JSX.Element;
-  impactHaptic?: undefined | 'medium' | 'heavy' | 'light';
   title?: string;
   busy?: boolean;
 }
 
 export function TrButton({
   startAdornment,
-  impactHaptic = 'light',
   endAdornment,
   disabled,
   onPress,
@@ -31,20 +27,11 @@ export function TrButton({
 }: TrButtonProps): JSX.Element {
   const theme = useTheme('TrButton');
 
-  /**
-   * Capture press in order to provide haptic feedback
-   */
-  function onInternalPress(e: GestureResponderEvent): void {
-    if (disabled || busy) return;
-    if (impactHaptic) Haptics.impact(impactHaptic);
-    if (onPress) onPress(e);
-  }
-
   return (
     <TouchableOpacity
       activeOpacity={disabled ? 0.5 : 0.2}
       disabled={disabled || busy}
-      onPress={onInternalPress}
+      onPress={onPress}
       style={[theme.root, { opacity: disabled ? 0.5 : 1 }, style]}
       key={disabled ? 0 : 1}
       {...props}
