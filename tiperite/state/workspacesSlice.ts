9cb3ce0015f0d0a96cb71f77f04b94164568ff53
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-11-07 16:09:40 -0800

    Replace set() with initialize()

--- tiperite/state/workspacesSlice.ts
+++ tiperite/state/workspacesSlice.ts
@@ -1,11 +1,20 @@
+import { StorageFileWorkspace, WorkspacesState } from '../types';
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { WorkspacesState } from '../types';
 
 export const workspacesSlice = createSlice({
   initialState: null as WorkspacesState | null,
   reducers: {
-    set(_, action: PayloadAction<WorkspacesState>): WorkspacesState {
-      return action.payload;
+    initialize(
+      _,
+      action: PayloadAction<StorageFileWorkspace[]>,
+    ): WorkspacesState {
+      return {
+        allIds: action.payload.map((workspace) => workspace.id),
+        byId: action.payload.reduce((byId, workspace) => {
+          byId[workspace.id] = workspace;
+          return byId;
+        }, {} as WorkspacesState['byId']),
+      };
     },
   },
   name: 'workspaces',
