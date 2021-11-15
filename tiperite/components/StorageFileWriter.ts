db760e80ebed6065d59529f984195f6806a2f45c
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-11-14 16:37:43 -0800

    Throttle to 1 write per 250ms

--- tiperite/components/StorageFileWriter.ts
+++ tiperite/components/StorageFileWriter.ts
@@ -5,22 +5,29 @@ import { useSelector } from '../hooks/useSelector';
 import { StorageFile } from '../utils/StorageFile';
 import React from 'react';
 
+/**
+ * Listen for state change to write to storage. Throttle to 1 write per 250ms.
+ */
 export function StorageFileWriter(): null {
   const credentials = useSelector(selectCredentials);
   const workspaces = useSelector(selectWorkspaces);
+  const timeout = React.useRef<NodeJS.Timeout>();
   const config = useSelector(selectConfig);
 
   React.useEffect(() => {
     if (!credentials || !workspaces) return;
+    clearTimeout(timeout.current as NodeJS.Timeout);
 
-    const data = StorageFile.getData();
+    timeout.current = setTimeout(() => {
+      const data = StorageFile.getData();
 
-    StorageFile.setData({
-      ...data,
-      credentials: Object.values(credentials.byId),
-      workspaces: Object.values(workspaces.byId),
-      config,
-    });
+      StorageFile.setData({
+        ...data,
+        credentials: Object.values(credentials.byId),
+        workspaces: Object.values(workspaces.byId),
+        config,
+      });
+    }, 250);
   }, [credentials, workspaces, config]);
 
   return null;
