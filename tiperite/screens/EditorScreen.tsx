07a9e45023ec8d9d1e583ac773a0349bbab67dc0
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2021-12-24 19:29:27 -0800

    Save doc's changes

--- tiperite/screens/EditorScreen.tsx
+++ tiperite/screens/EditorScreen.tsx
@@ -1,12 +1,18 @@
-import { StackNavigatorScreenProps, EncryptedDocBody } from '../types';
+import { selectNonNullableDocs, docsSlice } from '../state/docsSlice';
 import { selectNonNullableWorkspaces } from '../state/workspacesSlice';
-import { selectNonNullableDocs } from '../state/docsSlice';
 import { View, TextInput } from 'react-native';
 import { useTrSelector } from '../hooks/useTrSelector';
 import { useTheme } from '../hooks/useTheme';
 import { TrCrypto } from '../utils/TrCrypto';
+import { TrAlert } from '../utils/TrAlert';
+import { store } from '../state/store';
 import { FS } from '../utils/FS';
 import React from 'react';
+import {
+  StackNavigatorScreenProps,
+  EncryptedDocBody,
+  EncryptedDocMeta,
+} from '../types';
 
 /**
  * An editor for a doc's content
@@ -21,9 +27,15 @@ export function EditorScreen({
   const docs = useTrSelector(selectNonNullableDocs);
   const doc = docs.byId[route.params.docId];
 
+  const contentRef = React.useRef(content);
+  contentRef.current = content;
+
   // Load doc's content
   React.useEffect(() => {
-    FS.readFile(`/workspaces/${workspace.id}/docs/${doc.id}.body.json`)
+    const metaPath = `/workspaces/${workspace.id}/docs/${doc.id}.meta.json`;
+    const bodyPath = `/workspaces/${workspace.id}/docs/${doc.id}.body.json`;
+
+    FS.readFile(bodyPath)
       .then((data) => {
         if (!data) throw Error('Missing file');
         const body = JSON.parse(data) as EncryptedDocBody;
@@ -37,6 +49,38 @@ export function EditorScreen({
         setContent(blocks.join('\n\n'));
       })
       .catch(console.error);
+
+    // Save doc's changes
+    return () => {
+      const updatedAt = new Date().toISOString();
+      store.dispatch(docsSlice.actions.update({ ...doc, updatedAt }));
+
+      Promise.all(
+        contentRef.current.split('\n\n').map((block) => {
+          return TrCrypto.encrypt(block, workspace.keys);
+        }),
+      )
+        .then((blocks) => {
+          const body: EncryptedDocBody = { updatedAt, blocks };
+          return FS.writeFile(bodyPath, JSON.stringify(body, null, 2));
+        })
+        .then(() => {
+          return TrCrypto.encrypt(JSON.stringify(doc.headers), workspace.keys);
+        })
+        .then((headers) => {
+          const encryptedMeta: EncryptedDocMeta = {
+            createdAt: doc.createdAt,
+            updatedAt,
+            headers,
+            id: doc.id,
+          };
+          return FS.writeFile(metaPath, JSON.stringify(encryptedMeta, null, 2));
+        })
+        .catch((err) => {
+          console.error(err);
+          TrAlert.alert('Could not save changes');
+        });
+    };
   }, []);
 
   return (
