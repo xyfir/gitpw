4faadebd6c3edbfe8e6870cf6dba7a1ad8ee0cc1
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2022-05-28 21:38:32 -0700

    Add getUnlockedFileMap

--- src/commands/unlockCommand.ts
+++ src/commands/unlockCommand.ts
@@ -1,27 +1,22 @@
 import { GpwFileMap, GpwFile } from '../types';
 import { writeFile, readJSON } from 'fs-extra';
+import { getUnlockedFileMap } from '../utils/getUnlockedFileMap';
 import { getGpwPath } from '../utils/getGpwPath';
 import { getSession } from '../utils/getSession';
 import { TrCrypto } from '../utils/TrCrypto';
 import { getPath } from '../utils/getPath';
-import path from 'path';
 
 export async function unlockCommand(): Promise<void> {
   // Get session
   const session = await getSession();
 
   // Get encrypted-decrypted file name/path map
-  const map: GpwFileMap = await readJSON(path.join(getGpwPath('map.json')));
+  const map: GpwFileMap = await getUnlockedFileMap(session.unlocked_keychain);
 
   // Decrypt contents
-  for (const [id, filepath] of Object.entries(map)) {
-    // Decrypt map
-    map[id] = await TrCrypto.decrypt(filepath, session.unlocked_keychain);
-
+  for (const id of Object.keys(map)) {
     // Read encrypted file and decrypt its content
-    const file: GpwFile = await readJSON(
-      path.join(getGpwPath(`files/${id}.json`)),
-    );
+    const file: GpwFile = await readJSON(getGpwPath(`files/${id}.json`));
     const content = await Promise.all(
       file.content.map((c) => TrCrypto.decrypt(c, session.unlocked_keychain)),
     ).then((c) => c.join(''));
@@ -29,6 +24,4 @@ export async function unlockCommand(): Promise<void> {
     // Write decrypted file
     await writeFile(getPath(map[id]), content);
   }
-
-  return;
 }
---
+++ src/utils/getUnlockedFileMap.ts
@@ -0,0 +1,22 @@
+import { GpwUnlockedKeychain, GpwFileMap } from '../types';
+import { getGpwPath } from './getGpwPath';
+import { TrCrypto } from './TrCrypto';
+import { readJSON } from 'fs-extra';
+
+/**
+ * Load the file map from the local .gitpw directory and decrypt it
+ */
+export async function getUnlockedFileMap(
+  unlockedKeychain: GpwUnlockedKeychain,
+): Promise<GpwFileMap> {
+  // Get encrypted-decrypted file name/path map
+  const map: GpwFileMap = await readJSON(getGpwPath('map.json'));
+  if (!map) return {};
+
+  // Decrypt filepaths
+  for (const [id, filepath] of Object.entries(map)) {
+    map[id] = await TrCrypto.decrypt(filepath, unlockedKeychain);
+  }
+
+  return map;
+}
