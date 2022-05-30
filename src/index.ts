5df401aaff7a130c2a1e415c29cc263c163006a3
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2022-05-29 22:34:49 -0700

    Add saveCommand

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { initializeCommand } from './commands/initializeCommand';
 import { unlockCommand } from './commands/unlockCommand';
 import { lockCommand } from './commands/lockCommand';
+import { saveCommand } from './commands/saveCommand';
+
+type Commands = 'initialize' | 'unlock' | 'lock' | 'init' | 'save';
 
 (async () => {
   try {
-    switch (process.argv[2] as 'initialize' | 'unlock' | 'lock' | 'init') {
+    switch (process.argv[2] as Commands) {
       case 'initialize':
       case 'init':
         await initializeCommand();
@@ -14,6 +17,9 @@ import { lockCommand } from './commands/lockCommand';
         break;
       case 'lock':
         await lockCommand();
+        break;
+      case 'save':
+        await saveCommand();
         break;
       default:
         throw Error('Invalid command');
