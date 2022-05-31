b3f0ebc6ddf85ce38b42323534ac772bc99f2e4b
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2022-05-30 20:16:08 -0700

    Filter tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   testEnvironment: 'node',
+  testMatch: ['**/src/tests/**/*.test.ts'],
   preset: 'ts-jest',
 };
