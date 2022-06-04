f9fdff96c0fa5c6d1d06f8427ba8fc49b82fc2c4
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2022-06-04 10:06:42 -0700

    Test multikey keychains

--- src/tests/utils/GpwCrypto.test.ts
+++ src/tests/utils/GpwCrypto.test.ts
@@ -53,3 +53,75 @@ test('GpwCrypto with XChaCha20-Poly1305', async () => {
   expect(ciphertext).not.toBe(plaintext);
   expect(decrypted).toBe(plaintext);
 });
+
+test('GpwCrypto with AES-256-GCM => XChaCha20-Poly1305', async () => {
+  const plaintext = 'Hello, World!';
+  const aesPasskey = await GpwPBKDF2.deriveKey(
+    'password',
+    GpwPBKDF2.generateSalt(),
+    GpwPBKDF2.generateIterations(),
+  );
+  const xchaPasskey = await GpwPBKDF2.deriveKey(
+    'password2',
+    GpwPBKDF2.generateSalt(),
+    GpwPBKDF2.generateIterations(),
+  );
+
+  const keychain: GpwKeychain = {
+    created_at: new Date().toISOString(),
+    keys: [
+      {
+        type: 'AES-256-GCM',
+        data: aesPasskey,
+      },
+      {
+        type: 'XChaCha20-Poly1305',
+        data: xchaPasskey,
+      },
+    ],
+  };
+
+  const ciphertext = await GpwCrypto.encrypt(plaintext, keychain);
+  const ciphertext2 = await GpwCrypto.encrypt(plaintext, keychain);
+  const decrypted = await GpwCrypto.decrypt(ciphertext, keychain);
+
+  expect(ciphertext).not.toBe(ciphertext2);
+  expect(ciphertext).not.toBe(plaintext);
+  expect(decrypted).toBe(plaintext);
+});
+
+test('GpwCrypto with XChaCha20-Poly1305 => AES-256-GCM', async () => {
+  const plaintext = 'Hello, World!';
+  const aesPasskey = await GpwPBKDF2.deriveKey(
+    'password',
+    GpwPBKDF2.generateSalt(),
+    GpwPBKDF2.generateIterations(),
+  );
+  const xchaPasskey = await GpwPBKDF2.deriveKey(
+    'password2',
+    GpwPBKDF2.generateSalt(),
+    GpwPBKDF2.generateIterations(),
+  );
+
+  const keychain: GpwKeychain = {
+    created_at: new Date().toISOString(),
+    keys: [
+      {
+        type: 'XChaCha20-Poly1305',
+        data: xchaPasskey,
+      },
+      {
+        type: 'AES-256-GCM',
+        data: aesPasskey,
+      },
+    ],
+  };
+
+  const ciphertext = await GpwCrypto.encrypt(plaintext, keychain);
+  const ciphertext2 = await GpwCrypto.encrypt(plaintext, keychain);
+  const decrypted = await GpwCrypto.decrypt(ciphertext, keychain);
+
+  expect(ciphertext).not.toBe(ciphertext2);
+  expect(ciphertext).not.toBe(plaintext);
+  expect(decrypted).toBe(plaintext);
+});
