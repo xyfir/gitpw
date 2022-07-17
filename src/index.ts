ef531ffd3ca34be693c786e5bde847b5a7a0d95a
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2022-07-17 16:36:23 -0700

    Use yargs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,58 @@
-import type { Command } from './types';
+import type { GpwKeyType, Argv } from './types';
 import { runCommand } from './commands/runCommand';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs';
 
-(async () => {
-  try {
-    await runCommand(process.argv[2] as Command);
-    process.exit(0);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-})();
+yargs(hideBin(process.argv))
+  .option('password', {
+    description: 'Password(s) to encrypt/decrypt with, in order',
+    global: true,
+    alias: 'p',
+    array: true,
+  })
+  .command(
+    'init',
+    'Initialize a gitpw and git repository',
+    (yargs) =>
+      yargs
+        .option('encryption', {
+          description: 'Encryption method(s) to use, in order of passwords',
+          choices: ['XChaCha20-Poly1305', 'AES-256-GCM'] as GpwKeyType[],
+          alias: 'e',
+          array: true,
+        })
+        .option('vscode', {
+          description: 'Initialize a VSCode workspace',
+          boolean: true,
+          default: false,
+        }),
+    (argv) => runCommand('init', undefined, argv as Argv<'init'>),
+  )
+  .command(
+    'save',
+    'Encrypt and track plaintext changes to the gitpw repo',
+    () => undefined,
+    (argv) => runCommand('save', undefined, argv as Argv<'save'>),
+  )
+  .command(
+    'lock',
+    'The same as the "save" command, but it wipes plaintext files after',
+    () => undefined,
+    (argv) => runCommand('lock', undefined, argv as Argv<'lock'>),
+  )
+  .command(
+    'session',
+    'Start an authenticated session to easily run multiple commands',
+    () => undefined,
+    (argv) => runCommand('session', undefined, argv as Argv<'session'>),
+  )
+  .command(
+    'unlock',
+    'Decrypt gitpw repo, overwriting existing plaintext files',
+    () => undefined,
+    (argv) => runCommand('unlock', undefined, argv as Argv<'unlock'>),
+  )
+  .demandCommand(1)
+  .global('password')
+  .help()
+  .parse();
