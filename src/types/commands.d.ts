472422655c51cfe7c7c23be3f13c7c8c1ae1d6cd
Mr. Xyfir <MrXyfir@users.noreply.github.com>
2022-07-17 16:35:42 -0700

    Add argv for commands

--- src/types/commands.d.ts
+++ src/types/commands.d.ts
@@ -1,4 +1,4 @@
-import type { GpwUnlockedKeychain, GpwRepoManifest } from '.';
+import type { GpwUnlockedKeychain, GpwRepoManifest, GpwKeyType } from '.';
 
 /**
  * Authenticated session data
@@ -16,3 +16,28 @@ export type IntrasessionCommand = 'unlock' | 'lock' | 'save';
  * All command types
  */
 export type Command = IntrasessionCommand | 'session' | 'init';
+
+/**
+ * A command->argv map. Everything is optional.
+ */
+export type CommandsArgv = {
+  // [command]: argv
+  session?: { password?: string[] };
+  unlock?: { password?: string[] };
+  init?: {
+    encryption?: GpwKeyType[];
+    password?: string[];
+    vscode?: boolean;
+  };
+  save?: { password?: string[] };
+  lock?: { password?: string[] };
+};
+
+/**
+ * argv for a specific command name
+ *
+ * @example Argv
+ * @example Argv<'save'>
+ */
+export type Argv<K extends keyof CommandsArgv = keyof CommandsArgv> =
+  CommandsArgv[K];
